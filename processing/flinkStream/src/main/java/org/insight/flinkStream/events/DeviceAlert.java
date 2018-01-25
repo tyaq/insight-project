15d1e2b34deeaa80a0520bba827745d1a0fc3659
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2018-01-25 15:03:20 -0500

    Fix error

--- processing/flinkStream/src/main/java/org/insight/flinkStream/events/DeviceAlert.java
+++ processing/flinkStream/src/main/java/org/insight/flinkStream/events/DeviceAlert.java
@@ -1,41 +1,5 @@
 package org.insight.flinkStream.events;
 
 public class DeviceAlert {
-    private int deviceID;
 
-    public TemperatureAlert(int rackID) {
-      this.rackID = rackID;
-    }
-
-    public TemperatureAlert() {
-      this(-1);
-    }
-
-    public void setRackID(int rackID) {
-      this.rackID = rackID;
-    }
-
-    public int getRackID() {
-      return rackID;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (obj instanceof TemperatureAlert) {
-        TemperatureAlert other = (TemperatureAlert) obj;
-        return rackID == other.rackID;
-      } else {
-        return false;
-      }
-    }
-
-    @Override
-    public int hashCode() {
-      return rackID;
-    }
-
-    @Override
-    public String toString() {
-      return "TemperatureAlert(" + getRackID() + ")";
-    }
 }
