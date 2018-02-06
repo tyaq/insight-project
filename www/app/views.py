aaf3fa48bbeadde5b43494381adf213fff28cce7
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2018-02-06 16:32:39 -0500

    add freeboard to static

--- www/app/views.py
+++ www/app/views.py
@@ -7,51 +7,48 @@ from app import app
 # importing Cassandra modules from the driver we just installed
 from cassandra.cluster import Cluster
 
-# Setting up connections to cassandra
-
-# Change the bolded text to your seed node public dns (no < or > symbols but keep quotations. Be careful to copy quotations as it might copy it as a special character and throw an error. Just delete the quotations and type them in and it should be fine. Also delete this comment line
 cluster = Cluster(['ec2-35-153-150-183.compute-1.amazonaws.com'])
 
-# Change the bolded text to the keyspace which has the table you want to query. Same as above for < or > and quotations. Also delete this comment line
+# Setting up connections to cassandra
 session = cluster.connect('hypespace')
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  return app.send_static_file('static/freeboard/index.html')
 
 @app.route('/api/status')
 def get_status():
-       stmt = "SELECT * FROM status"
+       stmt = 'SELECT * FROM status'
        print(stmt)
        response = session.execute(stmt)
        response_list = []
        for val in response:
             response_list.append(val)
-       jsonresponse = [{"deviceID": x.deviceid.replace('\"', ''), "door-open": x.dooropen, "defrosted": x.defrosted, "efficiency": x.efficiency} for x in response_list]
+       jsonresponse = [{'deviceID': x.deviceid.replace('\"', ''), 'door-open': x.dooropen, 'defrosted': x.defrosted, 'efficiency': x.efficiency} for x in response_list]
        return jsonify(status=jsonresponse)
 
 @app.route('/api/status/<id>')
 def get_door(id):
        id='"'+id+'"'
-       stmt = "SELECT * FROM status WHERE deviceID=%s"
+       stmt = 'SELECT * FROM status WHERE deviceID=%s'
        print(stmt % id)
        response = session.execute(stmt,parameters=[id])
        response_list = []
        for val in response:
             response_list.append(val)
-       jsonresponse = [{"deviceID": x.deviceid.replace('\"', ''), "door-open": x.dooropen, "defrosted": x.defrosted, "efficiency": x.efficiency} for x in response_list]
+       jsonresponse = [{'deviceID': x.deviceid.replace('\"', ''), 'door-open': x.dooropen, 'defrosted': x.defrosted, 'efficiency': x.efficiency} for x in response_list]
        return jsonify(status=jsonresponse)
 
 @app.route('/api/timeline/<id>')
 def get_timeline(id):
        id='"'+id+'"'
-       stmt = "SELECT * FROM timeline WHERE deviceID=%s"
+       stmt = 'SELECT * FROM timeline WHERE deviceID=%s'
        print(stmt % id)
        response = session.execute(stmt,parameters=[id])
        response_list = []
        for val in response:
             response_list.append(val)
-       jsonresponse = [{"deviceID": x.deviceid.replace('\"', ''), "time_stamp": x.time_stamp, "sensorName1": x.sensorname1, "sensorValue1": x.sensorvalue1, "sensorName2": x.sensorname2, "sensorValue2": x.sensorvalue2,} for x in response_list]
+       jsonresponse = [{'deviceID': x.deviceid.replace('\"', ''), 'time_stamp': x.time_stamp, 'sensorName1': x.sensorname1, 'sensorValue1': x.sensorvalue1, 'sensorName2': x.sensorname2, 'sensorValue2': x.sensorvalue2,} for x in response_list]
        return jsonify(timeline=jsonresponse)
 
